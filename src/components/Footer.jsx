f0f77247dbd62d58141a631f370d0ef655c9d048
Oscar Moran <oscar91moran@gmail.com>
2024-03-22 11:58:28 +0000

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,34 @@
+import Link from "next/link";
+import style from "../styles/footer.module.css";
+
 export default function Footer() {
-  return <footer>Film-focus Footer</footer>;
+  return (
+    <footer className={style.footer}>
+      <section className={style.section}>
+        <h3>Page created by: </h3>
+        <ul>
+          <li className={style.il}>
+            <Link target="_blank" href="https://github.com/Mike7704">
+              Michael Cowley
+            </Link>
+          </li>
+          <li className={style.il}>
+            <Link target="_blank" href="https://github.com/Lima7048">
+              Salima Bryce
+            </Link>
+          </li>
+          <li className={style.il}>
+            <Link target="_blank" href="#">
+              Yonas Bogale
+            </Link>
+          </li>
+          <li className={style.il}>
+            <Link target="_blank" href="https://github.com/91oscar">
+              Oscar Moran
+            </Link>
+          </li>
+        </ul>
+      </section>
+    </footer>
+  );
 }
