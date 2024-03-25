5d3d805ac027b1e40a0388cc6aac602431e82caf
Oscar Moran <oscar91moran@gmail.com>
2024-03-25 10:43:32 +0000

    componente SearchBar doesnt'work

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import homeStyle from "@/styles/home.module.css";
+import SearchBar from "@/components/SearchBar";
+
 
 export default function Home() {
   return (
     <div className={homeStyle.container}>
       <h1>Home page</h1>
+      <SearchBar />
       <p className="button">Search bar</p>
       <div>
         <h3>Latest Movies</h3>
