6fa276b742592a511363c9ddf7ccca88e4366729
Mike7704 <Michael.cowley2001@gmail.com>
2024-03-26 15:50:28 +0000

    Made it so search bar shows results on the catalogue page.

--- src/app/movie-catalogue/page.jsx
+++ src/app/movie-catalogue/page.jsx
@@ -3,8 +3,9 @@ import { useState, useEffect } from "react";
 import catalogueStyle from "@/styles/catalogue.module.css";
 import MovieCard from "@/components/MovieCard";
 import Link from "next/link";
+import SearchBar from "@/components/SearchBar";
 
-export default function Catalogue() {
+export default function Catalogue({ searchParams }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -12,19 +13,28 @@ export default function Catalogue() {
   }, []);
 
   async function getMovies() {
-    const res = await fetch(`/api/movies`);
-    const moviesRes = await res.json();
+    let res;
+    let moviesRes;
+    if (searchParams.query) {
+      res = await fetch(`https://api.themoviedb.org/3/search/movie?query=${searchParams.query}&api_key=5ce82a49ce51b8b8835fd081ca4e815a`);
+    } else {
+      res = await fetch(`/api/movies`);
+    }
+    moviesRes = await res.json();
     setMovies(moviesRes.results);
   }
 
   return (
     <main className={catalogueStyle.container}>
       <h2>Movies:</h2>
+      <SearchBar />
       <div className={catalogueStyle.movies_container}>
         {movies.map((movie) => (
-          <Link href={`/movie/${movie.id}`}>
-            <MovieCard movie={movie} />
-          </Link>
+          <div key={movie.id}>
+            <Link href={`/movie/${movie.id}`}>
+              <MovieCard movie={movie} />
+            </Link>
+          </div>
         ))}
       </div>
     </main>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,45 +1,20 @@
 "use client";
-import React, { useState } from "react";
 import { ImSearch } from "react-icons/im";
 import style from "@/styles/search_bar.module.css";
+import { redirect } from "next/navigation";
 
 export default function SearchBar() {
-  const [value, setValue] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
-  const apiKey = process.env.TMDB_API_KEY;
-  const handleSearch = async () => {
-    try {
-      const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${value}&api_key=5ce82a49ce51b8b8835fd081ca4e815a`);
-      const data = await response.json();
-      console.log(apiKey);
-      console.log(data);
-      setSearchResults(data.results.rows);
-    } catch (error) {
-      console.error("Error getting search results:", error);
-    }
-  };
+  function handleSearch(formData) {
+    const searchQuery = formData.get("searchQuery");
+    redirect(`/movie-catalogue?query=${searchQuery}`);
+  }
 
   return (
-    <div className={style.div}>
+    <form action={handleSearch} className={style.div}>
       <div className={style.div2}>
         <ImSearch />
       </div>
-      <input
-        type="text"
-        placeholder="Search..."
-        value={value}
-        onClick={handleSearch}
-        onChange={(e) => setValue(e.target.value)}
-        className={style.input}
-      />
-      {/* Show search results */}
-      {searchResults && (
-        <ul>
-          {searchResults.map((result) => (
-            <li key={result.id}>{result.title}</li>
-          ))}
-        </ul>
-      )}
-    </div>
+      <input type="text" id="searchQuery" name="searchQuery" placeholder="Search..." />
+    </form>
   );
 }
