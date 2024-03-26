8a9fa345e774402bddd6d046dfe7f7d8dd4cbcea
salima <brycesalima@gmail.com>
2024-03-26 11:59:59 +0000

    salima wheel picker first stage

--- src/app/movie-picker/page.jsx
+++ src/app/movie-picker/page.jsx
@@ -1,9 +1,11 @@
 import wheelStyle from "@/styles/wheel.module.css";
+import WheelComponent from "@/components/WheelComponent";
 
 export default function Picker() {
   return (
     <main className={wheelStyle.container}>
       <p>Welcome to Picker</p>
+      <WheelComponent />
     </main>
   );
 }
